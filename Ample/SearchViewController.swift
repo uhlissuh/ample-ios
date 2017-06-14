d3f36a4b1f44517d4adc4da6b519032f74e28662
Alissa Sobo <alissasobo@gmail.com>
2017-06-14 12:12:07 -0700

    initial commit

---
+++ Ample/SearchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SearchViewController.swift
+//  Ample
+//
+//  Created by Alissa sobo on 6/13/17.
+//  Copyright © 2017 Alissa sobo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
