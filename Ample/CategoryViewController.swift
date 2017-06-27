fc830cdc3ea5cbc8abf303e04fbec3843d0f5aa1
Alissa Sobo <alissasobo@gmail.com>
2017-06-27 09:59:45 -0700

    add a url session to hit ample API for servicers, conv,rt to JSON and create a model from the retur data

--- Ample/CategoryViewController.swift
+++ Ample/CategoryViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+struct Servicer {
+    var name: String
+    var specialty: String
+}
+
 class CategoryViewController: UIViewController {
-    
-    
     var buttonTag: Int = 0
     
     enum ButtonType: Int {
@@ -19,11 +22,35 @@ class CategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        ButtonType(rawValue: sender.tag)!
+        getServicersForCategory(buttonTag: buttonTag) {(servicers) -> Void in
+            print(servicers)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func getServicersForCategory(buttonTag: Int, completionHandler: @escaping ([Servicer]) -> Void) {
+        let session = URLSession.shared
+        let url = URL(string: "http://localhost:8000/categories/" + String(buttonTag))!
+        let task = session.dataTask(with: url) { (data, response, error) -> Void in
+            do {
+                if let data = data {
+                    let servicersJSON = try JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
+                    let servicers = servicersJSON.map({(servicerJSON: [String: Any]) -> Servicer in
+                        return Servicer(
+                            name: servicerJSON["name"] as! String,
+                            specialty: servicerJSON["specialty"] as! String
+                        )
+                    })
+                    completionHandler(servicers)
+                }
+            } catch let parseError as NSError {
+                print("JSON Error \(parseError.localizedDescription)")
+            }
+        }
+        task.resume()
     }
     
 }
---
+++ Pods/Target Support Files/Pods-AmpleTests/Pods-AmpleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AmpleTests : NSObject
+@end
+@implementation PodsDummy_Pods_AmpleTests
+@end
