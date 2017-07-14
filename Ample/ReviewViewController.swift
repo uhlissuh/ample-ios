879236c853c6ad496979d0e9a5a95700ca5b8822
Alissa Sobo <alissasobo@gmail.com>
2017-07-14 11:34:19 -0700

    make category a searchcomplete field instead of a pickerview

--- Ample/ReviewViewController.swift
+++ Ample/ReviewViewController.swift
@@ -11,25 +11,20 @@ import MapKit
 import SearchTextField
 
 
-class ReviewViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource, MKLocalSearchCompleterDelegate {
-    
+class ReviewViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate, MKLocalSearchCompleterDelegate {
+    @IBOutlet weak var specialtyField: SearchTextField!
+    @IBOutlet weak var categoryField: SearchTextField!
     @IBOutlet weak var addressField: SearchTextField!
     @IBOutlet weak var nameField: SearchTextField!
     @IBOutlet weak var fatFriendlySlider: UISlider!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var reviewField: UITextView!
     @IBOutlet weak var skillSlider: UISlider!
-    @IBOutlet weak var specialtyField: UITextField!
     @IBOutlet weak var phoneField: UITextField!
-    @IBOutlet weak var categoryField: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     var searchCompleter = MKLocalSearchCompleter()
 
-
     var reviewContent: String = ""
-    
-    var categoryOptions = ["Medical", "Wellness", "Beauty", "Therapy", "Shopping", "Professional", "Fitness", "Restaurants" ]
-
     
     override func viewDidLoad() {
         
@@ -38,25 +33,17 @@ class ReviewViewController: UIViewController, UITextViewDelegate, UITextFieldDel
         scrollView.contentSize = CGSize(width: 375, height: 750)
         
         searchCompleter.delegate = self
-        
-        let pickerView = UIPickerView()
-        
-        pickerView.delegate = self
-        categoryField.inputView = pickerView
         reviewField.delegate = self
         nameField.delegate = self
         addressField.delegate = self
-        categoryField.inputView = pickerView
         phoneField.delegate = self
         submitButton.isEnabled = false
         nameField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
         addressField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
-
-        setupToolbarOnPicker()
         
         configureAddressField()
         configureNameField()
-        
+        configureCategoryField()
     }
     
 //    delegate methods for map autocomplete
@@ -106,7 +93,6 @@ class ReviewViewController: UIViewController, UITextViewDelegate, UITextFieldDel
         addressField.theme.cellHeight = 75
         addressField.theme.bgColor = UIColor.white
         addressField.highlightAttributes = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 18)]
-
         addressField.theme.font = UIFont.systemFont(ofSize: 18)
         addressField.userStoppedTypingHandler = {
             if (self.addressField.text != nil) {
@@ -121,65 +107,18 @@ class ReviewViewController: UIViewController, UITextViewDelegate, UITextFieldDel
         }
     }
     
-    fileprivate func configureCategory() {
+    fileprivate func configureCategoryField() {
+        categoryField.theme.cellHeight = 75
+        categoryField.theme.bgColor = UIColor.white
+        categoryField.highlightAttributes = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 18)]
+        categoryField.theme.font = UIFont.systemFont(ofSize: 18)
         
-    }
-
-//
-    func setupToolbarOnPicker() {
-        let toolBar = UIToolbar(frame: CGRect(x: 0, y: self.view.frame.size.height/6, width: self.view.frame.size.width, height: 40.0))
+        let categories = ["Medical", "Wellness", "Beauty", "Therapy", "Shopping", "Professional", "Fitness", "Restaurants" ]
         
-        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
-        
-        toolBar.barStyle = UIBarStyle.blackTranslucent
-        
-        toolBar.tintColor = UIColor.white
-        
-        toolBar.backgroundColor = UIColor.cyan
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(ReviewViewController.donePressed))
-        
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
-        
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width / 3, height: self.view.frame.size.height))
-        
-        label.font = UIFont(name: "Helvetica", size: 12)
-        
-        label.backgroundColor = UIColor.clear
-        
-        label.textColor = UIColor.white
-        
-        label.text = "Category"
-        
-        label.textAlignment = NSTextAlignment.center
-        
-        let textBtn = UIBarButtonItem(customView: label)
-        
-        toolBar.setItems([flexSpace,textBtn,flexSpace,doneButton], animated: true)
-        
-        categoryField.inputAccessoryView = toolBar
+        categoryField.filterStrings(categories)
 
     }
-    
-    func donePressed(_ sender: UIBarButtonItem) {
-        categoryField.resignFirstResponder()
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return categoryOptions.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return categoryOptions[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        categoryField.text = categoryOptions[row]
-    }
+
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
