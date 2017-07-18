d3a23e2cb175671e38e451c4961d709584dd79ea
Alissa Sobo <alissasobo@gmail.com>
2017-07-18 14:44:56 -0700

    make category text field search through categories and filter, display results in table view, rip out searchtextfield for this

--- Ample/SearchViewController.swift
+++ Ample/SearchViewController.swift
@@ -8,33 +8,54 @@
 
 import UIKit
 import MapKit
-import SearchTextField
 import MapKit
 import CoreLocation
 
 
-class SearchViewController: UIViewController, MKLocalSearchCompleterDelegate, CLLocationManagerDelegate {
+class SearchViewController: UIViewController, MKLocalSearchCompleterDelegate, CLLocationManagerDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var categorySearchField: SearchTextField!
-    @IBOutlet weak var locationSearchField: SearchTextField!
-   
-    var locationManager = CLLocationManager()
+    @IBOutlet weak var categorySearchField: UITextField!
+    @IBOutlet weak var locationSearchField: UITextField!
+    @IBOutlet weak var resultsTableView: UITableView!
     
+    var locationManager = CLLocationManager()
     var searchCompleter = MKLocalSearchCompleter()
- 
     var currentUserLocation = CLLocation()
+    var categoriesList: [String] = []
+    var filteredCategories: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+        searchCompleter.delegate = self
+        categorySearchField.delegate = self
+        resultsTableView.delegate = self
+        resultsTableView.dataSource = self
+        categorySearchField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        
+        setupLocationServices()
+        
+        getAllCategoryTitles { (categories) in
+            self.categoriesList = categories
+        }
+    }
+    
+    func editingChanged(){
+        filterContent(searchText: categorySearchField.text!)
+    }
+    
+    func filterContent(searchText: String) {
+        filteredCategories = categoriesList.filter { (category) -> Bool in
+            return category.lowercased().contains(searchText)
+        }
+        resultsTableView.reloadData()
+    }
+    
+    func setupLocationServices() {
         let authStatus = CLLocationManager.authorizationStatus()
         if authStatus == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
         }
-        searchCompleter.delegate = self
-
-        configureCategorySearchField()
-        configureLocationSearchField()
         
         locationManager.distanceFilter = 100.0  // In meters.
         locationManager.startUpdatingLocation()
@@ -43,69 +64,44 @@ class SearchViewController: UIViewController, MKLocalSearchCompleterDelegate, CL
             locationSearchField.text = "Near Current Location"
             locationSearchField.textColor = UIColor.blue
         }
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentUserLocation = locations[0]
     }
     
-    func configureCategorySearchField(){
-        categorySearchField.theme.cellHeight = 75
-        categorySearchField.theme.bgColor = UIColor.white
-        categorySearchField.highlightAttributes = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 18)]
-        categorySearchField.theme.font = UIFont.systemFont(ofSize: 18)
-        
-        getAllCategoryTitles() {(categories) in
-            self.categorySearchField.filterStrings(categories)
-        }
-        categorySearchField.itemSelectionHandler = { filteredResults, itemPosition in
-            let item = filteredResults[itemPosition]
-            self.categorySearchField.text = item.title
-        }
-    }
-    
-    func configureLocationSearchField(){
-        locationSearchField.startVisible = true
-        locationSearchField.theme.cellHeight = 75
-        locationSearchField.theme.bgColor = UIColor.white
-        locationSearchField.theme.font = UIFont.systemFont(ofSize: 18)
-        locationSearchField.highlightAttributes = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 18)]
-        locationSearchField.userStoppedTypingHandler = {
-            if (self.locationSearchField.text != nil) {
-                self.searchCompleter.queryFragment = self.locationSearchField.text!
-                self.locationSearchField.showLoadingIndicator()
-                
-            }
-        }
-        locationSearchField.itemSelectionHandler = { filteredResults, itemPosition in
-            let item = filteredResults[itemPosition]
-            if (item.subtitle != "") {
-                self.locationSearchField.text = item.title + ", " + item.subtitle!
-            } else {
-                self.locationSearchField.text = item.title
-            }
-        }
-    }
-    
     func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        let searchTextFieldItems = completer.results.map({ (searchCompletion: MKLocalSearchCompletion) -> SearchTextFieldItem in
-            SearchTextFieldItem(title: searchCompletion.title, subtitle: searchCompletion.subtitle)
-        })
-        self.locationSearchField.filterItems(searchTextFieldItems)
-        self.locationSearchField.stopLoadingIndicator()
+//        let searchTextFieldItems = completer.results.map({ (searchCompletion: MKLocalSearchCompletion) -> [String] in
+//            SearchTextFieldItem(title: searchCompletion.title, subtitle: searchCompletion.subtitle)
+//        })
+        //make this create cells for each item in searchtextfield items
     }
     
     func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
         print("THERE IS AN ERROR", error)
-        self.locationSearchField.stopLoadingIndicator()
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let categoriesVC = segue.destination as! CategoryViewController
-        categoriesVC.buttonTag = (sender as! UIButton).tag
     }
     
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultCell", for: indexPath)
+        cell.textLabel?.text = filteredCategories[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currentCell = tableView.cellForRow(at: indexPath)! as UITableViewCell
+        categorySearchField.text = currentCell.textLabel?.text
+        filteredCategories.removeAll()
+        resultsTableView.reloadData()
+        
+    }
+
     func getAllCategoryTitles(completionHandler: @escaping ([String]) -> Void){
         let url = URL(string: "http://localhost:8000/allcategorytitles")
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) -> Void in
